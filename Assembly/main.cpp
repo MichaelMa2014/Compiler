a630450d9765b2f2931e2c84d6a2f305d033468b
MichaelMa <Minima2014@iCloud.com>
2016-11-13 17:05:18 +0800

    Added an assembly hello world

---
+++ Assembly/main.cpp
@@ -0,0 +1,20 @@
+////
+////  main.cpp
+////  Assembly
+////
+////  Created by 马琛骁 on 11/13/16.
+////  Copyright © 2016 Michael Ma. All rights reserved.
+////
+//
+//#include <iostream>
+//#include <stdio.h>
+//
+//extern "C" long long GetRDTSC();
+//
+//int main(int, const char**)
+//{
+//    long long RDTSC1 = GetRDTSC();
+//    long long RDTSC2 = GetRDTSC();
+//    printf("Time-Stamp Counters: %lld - %lld\n", RDTSC1, RDTSC2);
+//    return 0;
+//}
