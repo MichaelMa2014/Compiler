97a87d6798f9b1258640200ab1e6b2c5177038af
MichaelMa <Minima2014@iCloud.com>
2016-11-15 23:08:27 +0800

    Add class IdentifierTable

---
+++ Compiler/identifier_table.cpp
@@ -0,0 +1,9 @@
+//
+//  identifier_table.cpp
+//  Compiler
+//
+//  Created by 马琛骁 on 11/15/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+#include "identifier_table.hpp"
---
+++ Compiler/identifier_table.hpp
@@ -0,0 +1,27 @@
+//
+//  identifier_table.hpp
+//  Compiler
+//
+//  Created by 马琛骁 on 11/15/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+#ifndef identifier_table_hpp
+#define identifier_table_hpp
+
+#include <vector>
+
+#include "init.hpp"
+
+class Identifier {
+    
+};
+
+class IdentifierTable {
+private:
+    vector<Identifier> table;
+public:
+    
+};
+
+#endif /* identifier_table_hpp */
