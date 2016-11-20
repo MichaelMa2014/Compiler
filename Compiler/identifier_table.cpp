4a5b6193e531e45d4c8c598d32f5138ae821c25d
MichaelMa <Minima2014@iCloud.com>
2016-11-20 14:06:33 +0800

    Implement Identifier Table

--- Compiler/identifier_table.cpp
+++ Compiler/identifier_table.cpp
@@ -7,3 +7,146 @@
 //
 
 #include "identifier_table.hpp"
+
+Constant::Constant(symbolNo k, int v) {
+    this -> kind = k;
+    this -> value = v;
+}
+
+IdType Constant::Type() {
+    return constId;
+}
+
+symbolNo Constant::Kind() {
+    return this -> kind;
+}
+
+int Constant::Value() {
+    return this -> value;
+}
+
+Variable::Variable(symbolNo k) {
+    this -> kind = k;
+}
+
+IdType Variable::Type() {
+    return varId;
+}
+
+symbolNo Variable::Kind() {
+    return this -> kind;
+}
+
+Matrix::Matrix(symbolNo k, int s) {
+    this -> kind = k;
+    this -> size = s;
+}
+
+IdType Matrix::Type() {
+    return matId;
+}
+
+symbolNo Matrix::Kind() {
+    return this -> kind;
+}
+
+int Matrix::Size() {
+    return this -> size;
+}
+
+Function::Function(symbolNo k, Parameter * p) {
+    this -> kind = k;
+    this -> parameters = p;
+}
+
+Function::~Function() {
+    delete parameters;
+    parameters = NULL;
+}
+
+IdType Function::Type() {
+    return funcId;
+}
+
+symbolNo Function::Kind() {
+    return this -> kind;
+}
+
+Parameter * Function::Parameters() {
+    return this -> parameters;
+}
+
+IdentifierTable::IdentifierTable() {
+    this -> table = Table();
+    this -> table.clear();
+}
+
+IdentifierTable::~IdentifierTable() {
+    for (Table::iterator it = table.begin(); it != table.end(); it++) {
+        delete it -> second;
+        it -> second = NULL;
+    }
+}
+
+const Identifier * IdentifierTable::Look(string name) {
+    if (table[name] == NULL) {
+        error(NO_DECLARE);
+    }
+    return table[name];
+}
+
+void IdentifierTable::EnterConstant(string name, symbolNo type, int value) {
+    if (table[name] != NULL) {
+        error(DOUBLE_DECLARE);
+    }
+    Identifier * temp = new Constant(type, value);
+    table[name] = temp;
+}
+
+void IdentifierTable::EnterVariable(string name, symbolNo type, int size){
+    if (table[name] != NULL) {
+        error(DOUBLE_DECLARE);
+    }
+    Identifier * temp;
+    if (size == 0) {
+        temp = new Variable(type);
+    }
+    else temp = new Matrix(type, size);
+    table[name] = temp;
+}
+
+void IdentifierTable::EnterFunction(string name, symbolNo type, Parameter * list) {
+    if (table[name] != NULL) {
+        error(DOUBLE_DECLARE);
+    }
+    Identifier * temp = new Function(type, list);
+    table[name] = temp;
+}
+
+void GIdentifierTable::EnterConstant(string name, symbolNo type, int value) {
+    if (table[name] != NULL) {
+        error(DOUBLE_DECLARE);
+    }
+    Identifier * temp = new Constant(type, value);
+    table[name] = temp;
+}
+
+void GIdentifierTable::EnterVariable(string name, symbolNo type, int size){
+    if (table[name] != NULL) {
+        error(DOUBLE_DECLARE);
+    }
+    Identifier * temp;
+    if (size == 0) {
+        temp = new Variable(type);
+    }
+    else temp = new Matrix(type, size);
+    table[name] = temp;
+}
+
+void GIdentifierTable::EnterFunction(string name, symbolNo type, Parameter * list) {
+    if (table[name] != NULL) {
+        error(DOUBLE_DECLARE);
+    }
+    Identifier * temp = new Function(type, list);
+    table[name] = temp;
+}
--- Compiler/identifier_table.hpp
+++ Compiler/identifier_table.hpp
@@ -9,19 +9,109 @@
 #ifndef identifier_table_hpp
 #define identifier_table_hpp
 
+#include <map>
 #include <vector>
 
 #include "init.hpp"
+#include "error.hpp"
+
+enum IdType {constId, varId, matId, funcId};
+
+typedef vector<symbolNo> Parameter;
 
 class Identifier {
-    
+    friend class IdentifierTable;
+    friend class GIdentifierTable;
+private:
+    int addr;
+public:
+    int Addr() {
+        return this -> addr;
+    }
+    virtual IdType Type() {
+        ERR("Identifier::Type() called");
+        exit(-1);
+    }
+    virtual symbolNo Kind() {
+        ERR("Identifier::Kind() called");
+        exit(-1);
+    }
+    virtual int Size() {
+        ERR("Identifier::Size() called");
+        exit(-1);
+    }
+    virtual int Value() {
+        ERR("Identifier::Value() called");
+        exit(-1);
+    }
+    virtual Parameter * Parameters() {
+        ERR("Identifier::Parameters() called");
+        exit(-1);
+    }
+};
+
+typedef map<string, Identifier *> Table;
+
+class Constant : public Identifier {
+private:
+    symbolNo kind;
+    int value;
+public:
+    Constant(symbolNo kind, int value);
+    virtual IdType Type();
+    virtual symbolNo Kind();
+    virtual int Value();
+};
+
+class Variable : public Identifier {
+private:
+    symbolNo kind;
+public:
+    Variable(symbolNo kind);
+    virtual IdType Type();
+    virtual symbolNo Kind();
+};
+
+class Matrix : public Identifier {
+private:
+    symbolNo kind;
+    int size;
+public:
+    Matrix(symbolNo kind, int size);
+    virtual IdType Type();
+    virtual symbolNo Kind();
+    virtual int Size();
+};
+
+class Function : public Identifier {
+private:
+    symbolNo kind;
+    Parameter * parameters;
+public:
+    Function(symbolNo kind, Parameter * parameters);
+    ~Function();
+    virtual IdType Type();
+    virtual symbolNo Kind();
+    virtual Parameter * Parameters();
 };
 
 class IdentifierTable {
-private:
-    vector<Identifier> table;
+protected:
+    Table table;
 public:
-    
+    IdentifierTable();
+    ~IdentifierTable();
+    const Identifier * Look(string name);
+    virtual void EnterConstant(string name, symbolNo type, int value);
+    virtual void EnterVariable(string name, symbolNo type, int size);
+    virtual void EnterFunction(string name, symbolNo type, Parameter * list);
+};
+
+class GIdentifierTable : public IdentifierTable {
+public:
+    virtual void EnterConstant(string name, symbolNo type, int value);
+    virtual void EnterVariable(string name, symbolNo type, int size);
+    virtual void EnterFunction(string name, symbolNo type, Parameter * list);
 };
 
 #endif /* identifier_table_hpp */
