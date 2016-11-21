e0edeeaf9277da365b2534b50379e09fe9b5f41d
MichaelMa <Minima2014@iCloud.com>
2016-11-21 15:37:19 +0800

    Use global variable for LexicalDecoder and Generator instead of passing them

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -13,17 +13,26 @@
 #include "grammar.hpp"
 #include "identifier_table.hpp"
 
+LexicalDecoder * ld;
+Generator * ge;
+vector<Quaternary *> table;
+
 int main(int argc, const char * argv[]) {
     init();
     
-    LexicalDecoder * ld = new LexicalDecoder();
-    IdentifierTable * id = new IdentifierTable();
-    GrammarDecoder * gd = new GrammarDecoder(ld, id, id);
+    ld = new LexicalDecoder();
     
-    int value = 99;
+    IdentifierTable * id = new IdentifierTable();
+    GrammarDecoder * gd = new GrammarDecoder(id, id);
+    
+    ge = new Generator(id);
     
     ld -> NextWord();
     gd -> Program();
+    
+    for (vector<Quaternary *>::iterator it = table.begin(); it != table.end(); it++) {
+        (* it) -> Print();
+    }
     
     return 0;
 }
