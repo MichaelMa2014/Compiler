eb8e732f9e4dc2f555ed65cb1700a84767604179
MichaelMa <Minima2014@iCloud.com>
2016-12-05 15:20:20 +0800

    Lay the groundwork for optimizing
    Not a good idea to have different files in different branches. I will
    create a pull request now and merge the basic version of the 2 files to
    master branch.

---
+++ Compiler/optimizer.cpp
@@ -0,0 +1,9 @@
+//
+//  optimizer.cpp
+//  Compiler
+//
+//  Created by 马琛骁 on 12/5/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+#include "optimizer.hpp"
---
+++ Compiler/optimizer.hpp
@@ -0,0 +1,16 @@
+//
+//  optimizer.hpp
+//  Compiler
+//
+//  Created by 马琛骁 on 12/5/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+#ifndef optimizer_hpp
+#define optimizer_hpp
+
+#include "init.hpp"
+#include "quaternary.hpp"
+
+
+#endif /* optimizer_hpp */
