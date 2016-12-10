8a29a52165e1c65d5783c8eca0cc593f666110f4
MichaelMa <Minima2014@iCloud.com>
2016-12-10 11:13:23 +0800

    Lay the groundwork

---
+++ Compiler/basic_block.cpp
@@ -0,0 +1,9 @@
+//
+//  basic_block.cpp
+//  Compiler
+//
+//  Created by 马琛骁 on 12/10/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+#include "basic_block.hpp"
---
+++ Compiler/basic_block.hpp
@@ -0,0 +1,22 @@
+//
+//  basic_block.hpp
+//  Compiler
+//
+//  Created by 马琛骁 on 12/10/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+#ifndef basic_block_hpp
+#define basic_block_hpp
+
+#include <iostream>
+
+#include "quaternary.hpp"
+
+class Block {
+private:
+    Table::iterator start;
+    Table::iterator end;
+};
+
+#endif /* basic_block_hpp */
